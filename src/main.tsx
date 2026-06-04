b6bcf4bc4971b23aafb78fef1afe09b930dd5fd2
Ana Marie <amtpiodos@gmail.com>
2026-06-04 21:31:20 +0800

    remove base ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { defineCustomElements as defineBaseUi } from '@jobtarget/base-ui/loader'
+// import { defineCustomElements as defineBaseUi } from '@jobtarget/base-ui/loader'
 import './index.css'
 import App from './App.tsx'
 
-defineBaseUi()
+// defineBaseUi()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
